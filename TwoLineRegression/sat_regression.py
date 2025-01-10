a9cc9d911864cd7ff5bab0c3f6ebc08dbda0d9d7
Adam Zheng <adam.ray.zheng@gmail.com>
2025-01-09 21:39:27 -0500

    slight modifications

--- TwoLineRegression/sat_regression.py
+++ TwoLineRegression/sat_regression.py
@@ -37,7 +37,7 @@ elif YEAR[0] == 2019 and YEAR[1] == "SAT":
     Y = [75.03, 70.68, 74.28, 79.50, 69.70, 71.92, 71.03, 69.38, 66.43, 79.13, 75.84, 63.68]
 
 elif YEAR[0] == 2023 and YEAR[1] == "ACT":
-    # ACT 2023, 50 STATES, HIGH SPEED
+    # ACT 2023, 15 STATES, HIGH SPEED
     #X1 = [17.4,19,16.8,18,25.8,24.4,26.7,25,26.6,18.5,20.7,16.6,22.5,24.5,22.1,19.7,18.3,18.1,17.8,25.1,24.5,26.4,24.3,19.3,17,19,17.4,18.3,16,25.1,24.5,19.3,25,16.9,18.4,17.9,17,19.8,23.6,24.6,17.6,19.9,17.9,18.2,18.9,23.1,24.6,24.2,20,18.3,17.9]
     X1 = [17.4, 18, 18.1, 17.8, 17, 17.4, 18.3, 16, 16.9, 17.9, 17, 17.9, 18.9, 18.3, 17.9]
     #X2 = [17.3,19.6,17.8,17.8,24.9,23.5,25.4,23.5,24.3,18.2,20.6,17.7,22.4,23.8,22.6,20.1,18.9,18.1,17.4,23.5,23.3,25.7,23.6,20.5,17.3,19.2,18.6,18.9,16.9,24.4,23.8,19.4,24.6,18.3,19.4,19,17.1,20.2,23.2,23.6,18.3,20.8,18,19.1,19.3,22.1,23.4,23.6,19.3,19.2,18.5]
@@ -96,11 +96,11 @@ plt.xlabel("% of High Speed Internet Access")
 
 # legend
 math_legend = plt.legend(
-    eng_plot, [f"English {YEAR[1]} Scores", f"Line of Best Fit (% High Speed Internet Access vs English {YEAR[1]} Scores)"],
+    eng_plot, [f"English {YEAR[1]} Scores", f"Line of Best Fit (English {YEAR[1]} Scores vs. % High Speed Internet Access)"],
     loc=1
 )
 eng_legend = plt.legend(
-    math_plot, [f"Math {YEAR[1]} Scores", f"Line of Best Fit (% High Speed Internet Access vs Math {YEAR[1]} Scores)"],
+    math_plot, [f"Math {YEAR[1]} Scores", f"Line of Best Fit (Math {YEAR[1]} Scores vs. % High Speed Internet Access)"],
     loc=2
 )
 corr_legend = plt.legend(
