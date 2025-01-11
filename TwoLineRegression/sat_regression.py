12ce0ed25eac757b0152e2c66e28e10dbce20399
Adam Zheng <adam.ray.zheng@gmail.com>
2025-01-10 20:09:18 -0500

    minor changes, added author of code

--- TwoLineRegression/sat_regression.py
+++ TwoLineRegression/sat_regression.py
@@ -1,3 +1,5 @@
+# programmed by Adam Zheng
+
 import numpy as np
 import matplotlib.pyplot as plt
 import math
@@ -9,7 +11,10 @@ X2 - Math
 Y - % High Speed Internet Access
 """
 
-YEAR = (2019, "ACT") # change year to change plot
+ACCESS_TO_HIGH_SPEED = "High Speed Internet Access"
+ACCESS_TO_ANY_SPEED = "Internet Access"
+
+YEAR = (2023, "ACT", ACCESS_TO_ANY_SPEED) # change year to change plot
 if YEAR[0] == 2023 and YEAR[1] == "SAT":
     # SAT 2023, HIGH SPEED
     X1 = [508, 512, 489, 495, 503, 494, 492, 489, 493, 458, 489, 478, 515, 497, 526]
@@ -29,16 +34,20 @@ elif YEAR[0] == 2019 and YEAR[1] == "SAT":
     Y = [75.03, 70.68, 74.28, 79.50, 69.70, 71.92, 71.03, 69.38, 66.43, 79.13, 75.84, 63.68]
 
 elif YEAR[0] == 2023 and YEAR[1] == "ACT":
-    # ACT 2023, 15 STATES, HIGH SPEED
+    # ACT 2023, 15 STATES
     
-    # commented below is for all 50 states
+    # commented below is for all 50 states, high speed
     # X1 = [17.4,19,16.8,18,25.8,24.4,26.7,25,26.6,18.5,20.7,16.6,22.5,24.5,22.1,19.7,18.3,18.1,17.8,25.1,24.5,26.4,24.3,19.3,17,19,17.4,18.3,16,25.1,24.5,19.3,25,16.9,18.4,17.9,17,19.8,23.6,24.6,17.6,19.9,17.9,18.2,18.9,23.1,24.6,24.2,20,18.3,17.9]
     # X2 = [17.3,19.6,17.8,17.8,24.9,23.5,25.4,23.5,24.3,18.2,20.6,17.7,22.4,23.8,22.6,20.1,18.9,18.1,17.4,23.5,23.3,25.7,23.6,20.5,17.3,19.2,18.6,18.9,16.9,24.4,23.8,19.4,24.6,18.3,19.4,19,17.1,20.2,23.2,23.6,18.3,20.8,18,19.1,19.3,22.1,23.4,23.6,19.3,19.2,18.5]
     # Y = [67.03,66.17,68.39,66.42,74.40,75.95,77.21,83.18,81.89,75.29,71.46,77.58,76.04,74.46,68.06,70.24,68.60,76.05,67.30,77.23,83.12,77.20,76.74,73.73,63.03,65.67,62.36,68.82,65.54,82.37,80.72,74.54,74.48,69.92,75.79,73.41,70.39,78.93,73.67,77.03,77.64,72.26,68.10,75.88,77.50,74.11,74.86,75.77,68.00,77.72,70.47]
     
     X1 = [17.4, 18, 18.1, 17.8, 17, 17.4, 18.3, 16, 16.9, 17.9, 17, 17.9, 18.9, 18.3, 17.9]
     X2 = [17.3, 17.8, 18.1, 17.4, 17.3, 18.6, 18.9, 16.9, 18.3, 19, 17.1, 18, 19.3, 19.2, 18.5]
-    Y = [67.03, 66.42, 76.05, 67.30, 63.03, 62.36, 68.82, 65.54, 69.92, 73.41, 70.39, 68.10, 77.50, 77.72, 70.47]
+    
+    if YEAR[2] == ACCESS_TO_HIGH_SPEED:
+        Y = [67.03, 66.42, 76.05, 67.30, 63.03, 62.36, 68.82, 65.54, 69.92, 73.41, 70.39, 68.10, 77.50, 77.72, 70.47]
+    elif YEAR[2] == ACCESS_TO_ANY_SPEED:
+        Y = [78.85,75.85,81.84,75.95,75.33,71.14,74.22,72.34,69.99,79.12,80.63,73.22,79.85,87.88,83.07]
 
 elif YEAR[0] == 2021 and YEAR[1] == "ACT": # NOT USED
     # ACT 2021, ALL STATES, HIGH SPEED
@@ -96,22 +105,22 @@ plt.ylim(YLIM_MIN, YLIM_MAX)  # % of internet access
 
 # labels
 TITLE_Y = f"% of students meeting Ontario provincial standards ({YEAR[0]})" if YEAR[1] == "EQAO" else f"{YEAR[1]} Section Scores for {min(len(Y), 50)} U.S. states ({YEAR[0]})"
-TITLE_X = "% of High Speed Internet Access"
+TITLE_X = f"% of {YEAR[2]}"
 TITLE = f"{TITLE_Y} vs. {TITLE_X}"
 
 YLABEL = f"% of students meeting Ontario provincial standards" if YEAR[1] == "EQAO" else f"Score on Specific {YEAR[1]} Section ({YEAR[0]})"
 
 plt.title(TITLE)
 plt.ylabel(YLABEL)
-plt.xlabel("% of High Speed Internet Access")
+plt.xlabel(f"% of {YEAR[2]}")
 
 # legend
 math_legend = plt.legend(
-    eng_plot, [f"English {YEAR[1]} Scores", f"Line of Best Fit (English {YEAR[1]} Scores vs. % High Speed Internet Access)"],
+    eng_plot, [f"English {YEAR[1]} Scores", f"Line of Best Fit (English {YEAR[1]} Scores vs. % {YEAR[2]})"],
     loc=1
 )
 eng_legend = plt.legend(
-    math_plot, [f"Math {YEAR[1]} Scores", f"Line of Best Fit (Math {YEAR[1]} Scores vs. % High Speed Internet Access)"],
+    math_plot, [f"Math {YEAR[1]} Scores", f"Line of Best Fit (Math {YEAR[1]} Scores vs. % {YEAR[2]})"],
     loc=2
 )
 corr_legend = plt.legend(
